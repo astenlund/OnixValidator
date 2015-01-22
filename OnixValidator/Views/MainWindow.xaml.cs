9eafaeb5a76ea56c4e2952b6acfb86a4c6134678
Andreas Stenlund (asd) <andreas.stenlund@tobii.com>
2015-01-22 16:00:53 +0100

    Discontinue the use of the ridiculous EventToCommand construction
    Instead opting for a more straight-forward approach using event handlers in
    code-behind, relaying to commands in the view model.

--- OnixValidator/Views/MainWindow.xaml.cs
+++ OnixValidator/Views/MainWindow.xaml.cs
@@ -1,10 +1,43 @@
 ﻿namespace OnixValidator.Views
 {
+    using System.Windows;
+    using System.Windows.Input;
+
+    using OnixValidator.ViewModels;
+
     public sealed partial class MainWindow
     {
+        private MainViewModel _viewModel;
+
         public MainWindow()
         {
+            Loaded += OnLoaded;
             InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            _viewModel = DataContext as MainViewModel;
+        }
+
+        private void OnDragEnter(object sender, DragEventArgs e)
+        {
+            _viewModel.DragEnterCommand.Execute(e.Data);
+        }
+
+        private void OnDragLeave(object sender, DragEventArgs e)
+        {
+            _viewModel.DragLeaveCommand.Execute(null);
+        }
+
+        private void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            _viewModel.DragLeaveCommand.Execute(null);
+        }
+
+        private void OnDrop(object sender, DragEventArgs e)
+        {
+            _viewModel.DropCommand.Execute(e.Data);
         }
     }
 }
